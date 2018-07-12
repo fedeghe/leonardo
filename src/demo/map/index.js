abd01b17a3a44e961e65b34c1f80001c86a165f4
Federico Ghedina <fedeghe@gmail.com>
2018-07-12 22:59:41 +0200

    map  f-m

--- src/demo/map/index.js
+++ src/demo/map/index.js
@@ -6,20 +6,25 @@ window.onload = function () {
 		height = size,
 		gridNum = 10,
 		tileSize = size / gridNum,
-		L = Leonardo(width, height, {ns : '*', target : target});
+		L = Leonardo(width, height, {ns : '*', target : target}),
+		id = 0;
 
 	var rect = L.rect(0, 0, tileSize),
 		circle = L.circle(tileSize / 2, tileSize / 2, tileSize / 2);
 
 	for (var i = 0; i < gridNum; i++) {
-		for (var j = 0; j < gridNum; j++) {
+		for (var j = 0, t; j < gridNum; j++) {
+			isCircle = Math.random() > 0.5;
 			L.add(
-				(Math.random() > 0.5 ? circle : rect)
+				(isCircle ? circle : rect)
 				.clone()
 				.move(
 					i * tileSize,
 					(j % gridNum) * tileSize
-				)
+				).attrs({
+					'data-element-id': 'ID'+id++,
+					'fill': isCircle ? '#f77' : '#47f'
+				})
 			);
 		}
 	}
