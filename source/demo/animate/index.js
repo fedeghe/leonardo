8f4df54c31a145dc3dfe2d176a1ef9952a43095f
fedeghe <fedeghe@gmail.com>
2020-07-07 17:25:14 +0200

    animate sample added

---
+++ source/demo/animate/index.js
@@ -0,0 +1,65 @@
+window.onload = function () {
+    var target1 = document.getElementById('trg1'),
+        target2 = document.getElementById('trg2');
+
+    (function () {
+        var width = 500,
+            height = 500,
+            min = width > height ? height : width,
+            wperc = width / 100,
+            hperc = height / 100,
+            mperc = min / 100,
+            
+            w = function(n) {return n * wperc; },
+            h = function(n) {return n * hperc; },
+            m = function(n) {return n * mperc; },
+
+            L = Leonardo(width, height, { ns: '*', target: target1 }),
+            circle = L.circle(0,0,10).setAttributes({
+                fill: '#f45'
+            }),
+            w2 = width / 2,
+            h2 = height / 2;
+        L.animate.cartesian(
+            circle,
+            function (x, t){
+                return w2 + w2 * Math.sin(t/4);
+            },
+            function (y, t){
+                return h2 + h2 * Math.cos(t/8);
+            }
+        )
+        L.append(circle).render();
+    })();
+
+    (function () {
+        var width = 500,
+            height = 500,
+            min = width > height ? height : width,
+            wperc = width / 100,
+            hperc = height / 100,
+            mperc = min / 100,
+            
+            w = function(n) {return n * wperc; },
+            h = function(n) {return n * hperc; },
+            m = function(n) {return n * mperc; },
+
+            L = Leonardo(width, height, { ns: '*', target: target2 }),
+            circle = L.circle(width / 2, height / 2, 10).setAttributes({
+                fill: '#34f'
+            });
+        L.animate.polar(
+            circle,
+            function (r, t){
+                return  min/2 * Math.sin(t/7);
+            },
+            function (y, t){
+                return t;
+            }
+        )
+        L.append(circle).render();
+    })();
+
+
+    // document.body.appendChild(L.downloadAnchor());
+};
