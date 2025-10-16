b7edccf07a0ddbb4f07dffa004fc35f805ba9c42
fedeghe <fedeghe@gmail.com>
2025-10-16 22:35:49 +0200

    uou

--- source/demo/uoullet/index.js
+++ source/demo/uoullet/index.js
@@ -13,35 +13,25 @@ window.onload = function () {
             // width = window.innerWidth - 50,
             // height = parseInt(width / prop, 10),
             Leo = Leonardo(width, height, { ns: '*', target: target }).setStyles({
-                backgroundColor: '#222222aa'
+                // backgroundColor: '#222222aa' 
             }),
             w = Leonardo.getScaler(width), //function (p) { return width * p / 100; },
             h = Leonardo.getScaler(height), //function (p) { return height * p / 100; },
             img = Leo.image(0, 0, width, height, './uoullet.jpg').setAttributes({ opacity: 0.4 }),
             fillStyle = {
-                "stroke-width": 4,
-                "stroke": '#fff',
+                "stroke-width": 2,
                 "stroke-opacity": 1,
                 "fill-opacity": 10,
                 "stroke-linejoin": "round",
-                fill: 'transparent'
+                "stroke": '#fff',
+                fill: '#ffffff55'
             },
             gridStyle = {...fillStyle, "stroke-width": 2, "stroke-opacity": 0.2,"stroke": '#fff',},
             boxStyle = {...fillStyle, "stroke-width": 2, "stroke-opacity": 0.2,"stroke": '#00f',},
-            // u1 = Leo.path(
-            //     Leo.pathBuild
-            //         .M(35, 50)
-            //         .L(35, 160)
-            // ).setAttributes(fillStyle),
-
+         
             crunch = Leo.positionCruncher(width, height, fillStyle),
             crunchClose = Leo.positionCruncher(width, height, fillStyle, true),
-            // grid = Leo.group(
-            //   Leo.line(0,10,1100,10),
-            //   Leo.line(0,30,1100,30),
-            //   Leo.line(0,100,1100,100),
-            //   Leo.line(0,120,1100,120),
-            // ).setAttributes(gridStyle);
+
             gap = 5,
             hGapMul = 4,
             wGapMul = 8,
@@ -98,7 +88,7 @@ window.onload = function () {
 
                     // .A(gap*wGapMul, gap*hGapMul, 0, 0, 0, gap, gap*hGapMul)
                     // .Z()
-                    ).setAttributes({...fillStyle, fill:'#55000077'}),
+                    ),
             o = Leo.path(
                 Leo.pathBuild
                     .M(gap, gap*hGapMul)
@@ -110,7 +100,6 @@ window.onload = function () {
                     .A(gap*wGapMul, gap*hGapMul, 0, 0, 0, bxWidth - gap*wGapMul, gap)
                     .L(gap*wGapMul, gap)
                     .A(gap*wGapMul, gap*hGapMul, 0, 0, 0, gap, gap*hGapMul)
-                    // .Z()
                     .M(gap*wGapMul+gap, gap*hGapMul)
                     .L(bxWidth-gap*wGapMul -gap, gap*hGapMul)
                     .A(gap, gap, 0,0,1,bxWidth-gap*wGapMul, gap*hGapMul + gap)
@@ -120,8 +109,9 @@ window.onload = function () {
                     .A(gap, gap, 0,0,1,gap*wGapMul, bxHeight - gap*hGapMul-gap)
                     .L(gap*wGapMul, gap*hGapMul+gap)
                     .A(gap, gap, 0,0,1,gap*wGapMul+gap, gap*hGapMul)
-
-            ).move(bxWidth, 0).setAttributes(({...fillStyle, fill:'#00550077'})),
+                    .Z()
+                    
+            ).move(bxWidth, 0),
             l1 = Leo.path(
                 Leo.pathBuild
                     .M(gap, gap*hGapMul)
@@ -137,10 +127,11 @@ window.onload = function () {
                     .A(10, gap*hGapMul, 0, 0, 1, gap*wGapMul + 10, gap)
                     .A(gap*wGapMul, gap*hGapMul, 0, 0, 0, gap, gap*hGapMul)
                     .Z()
-                    // .L(bxWidth - gap, gap*hGapMul)
-                    // .A(gap*wGapMul, gap*hGapMul, 0, 0, 0, bxWidth - gap*wGapMul, gap)
-            ).move(bxWidth*3, 0).setAttributes(({...fillStyle, fill:'#00005577'})),
+                    
+            ).move(bxWidth*3, 0),
             l2 = l1.clone().move(bxWidth*4, 0),
+
+            eMidDist = bxWidth*7/10,
             e = Leo.path(
                 Leo.pathBuild
                     .M(gap, gap*hGapMul)
@@ -152,12 +143,13 @@ window.onload = function () {
                     .A(wGapMul*gap/2, wGapMul*gap/2, 0, 0, 1, bxWidth - wGapMul*gap/2 - wGapMul*gap/2, bxHeight - hGapMul*gap)
                     .L(wGapMul*gap + gap, bxHeight - hGapMul*gap)
                     .A(gap, gap, 0, 0, 1, wGapMul*gap + gap - gap, bxHeight - hGapMul*gap - gap)
-                    .L(wGapMul*gap, bxHeight/2 + gap)
-                    .L(bxWidth/2, bxHeight/2 + gap)
-                    .L(bxWidth/2, bxHeight/2 + 3*gap)
-                    .A(2*gap, 3*gap, 0, 0, 0, bxWidth/2, bxHeight/2 - 3*gap)
-                    .L(bxWidth/2, bxHeight/2 - gap)
-                    .L(wGapMul*gap, bxHeight/2 - gap)
+
+                    .L(wGapMul*gap, bxHeight/2 + hGapMul*gap/2)
+                    .L(eMidDist, bxHeight/2 + hGapMul*gap/2)
+                    .L(eMidDist, bxHeight/2 + 3*gap)
+                    .A(2*gap, 3*gap, 0, 0, 0, eMidDist, bxHeight/2 - 3*gap)
+                    .L(eMidDist, bxHeight/2 - hGapMul*gap/2)
+                    .L(wGapMul*gap, bxHeight/2 - hGapMul*gap/2)
                     .L(wGapMul*gap, hGapMul*gap + gap)
                     .A(gap, gap, 0, 0, 1, wGapMul*gap+gap, hGapMul*gap)
                     .L(bxWidth - wGapMul*gap, hGapMul*gap)
@@ -183,29 +175,44 @@ window.onload = function () {
                     .L(wGapMul*gap*2 - gap+ 5*gap, bxHeight - gap)
                     .A(wGapMul*gap*2, gap+hGapMul*gap, 0,0,1, wGapMul*gap, bxHeight - hGapMul*gap - gap)
                     .L(wGapMul*gap, hGapMul*gap)
-                    .L(wGapMul*gap/2, hGapMul*gap + wGapMul*gap/2)
-                    .A(wGapMul*gap/2, wGapMul*gap/2, 0, 0, 1, gap, hGapMul*gap)
+                    .A(wGapMul*gap/2, wGapMul*gap/2, 0, 0, 0, wGapMul*gap/2, hGapMul*gap + wGapMul*gap/2)
+                    .L(gap, hGapMul*gap)
                     .Z()
                     
             ).move(bxWidth*6, 0)
-            u2 = u1.clone().move(bxWidth * 2, 0);
+            u2 = u1.clone().move(bxWidth * 2, 0),
 
-        // Leo.append(img);
-        Leo.append(
-            Leo.group(
+            g = Leo.group(
                 u1,
                 o,
                 u2,
                 l1, l2,
                 e,t
-            ).setAttributes(fillStyle),
-            bx1.move(0, 0),
-            bx2.move(150, 0),
-            bx3.move(300, 0),
-            bx4.move(450, 0),
-            bx5.move(600, 0),
-            bx6.move(750, 0),
-            bx7.move(900, 0),
+            // ).setAttributes(fillStyle);
+            ).setAttributes({...fillStyle, "stroke": '#000', fill: '#00000055'});
+
+        // Leo.append(img);
+        Leo.append(
+            g,
+            
+            // bx1.move(0, 0),
+            // bx2.move(150, 0),
+            // bx3.move(300, 0),
+            // bx4.move(450, 0),
+            // bx5.move(600, 0),
+            // bx6.move(750, 0),
+            // bx7.move(900, 0),
+        );
+        Leo.append(
+            
+            g
+            // bx1.move(0, 0),
+            // bx2.move(150, 0),
+            // bx3.move(300, 0),
+            // bx4.move(450, 0),
+            // bx5.move(600, 0),
+            // bx6.move(750, 0),
+            // bx7.move(900, 0),
         );
         // Leo.append(u1);
 
@@ -215,7 +222,7 @@ window.onload = function () {
 
         Leo.render();
         // Leo.positionInspector('[{r%x}, {r%y}],');
-        Leo.downloadAnchor('download', 'vitruvian', document.body);
+        Leo.downloadAnchor('download', 'uoullet', document.body);
 
         
     }
